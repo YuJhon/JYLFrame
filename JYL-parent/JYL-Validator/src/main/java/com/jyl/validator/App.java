f462578cabbab949a5c772c7055ed3b6bd61b540
YuJhon <3291371805@qq.com>
2016-03-30 18:15:52 +0800

    【新增】JYL框架首次提交【江宇】

---
+++ JYL-parent/JYL-Validator/src/main/java/com/jyl/validator/App.java
@@ -0,0 +1,13 @@
+package com.jyl.validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
